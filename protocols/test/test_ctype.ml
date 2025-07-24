3efd5e14b47b18db1b247e7b2ba0ca79f7ba857e
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2025-07-24 19:00:12 -0400

    Moved tests to inside protocols; migrated tests to Alcotest.

---
+++ protocols/test/test_ctype.ml
@@ -0,0 +1,52 @@
+open Protocols
+open C_type
+
+let test_split_array_type () : unit =
+  let test_cases = [
+    ("int *", Some ("int", "*"));
+    ("int", None);
+    ("unsigned int [8][8] *", Some ("unsigned int", "[8][8] *"));
+    ("int [128]", Some ("int", "[128]"));
+    ("float[1024]", Some ("float", "[1024]"));
+  ] in
+  List.iter (fun (given, expected) ->
+    Alcotest.check Alcotest.(option (pair string string)) ("split_array_type " ^ given) expected (split_array_type given)
+  ) test_cases
+
+let test_parse_dim () : unit =
+  Alcotest.check Alcotest.(list int) "parse_dim [8][8]" [8; 8] (parse_dim "[8][8]");
+  Alcotest.check Alcotest.(list int) "parse_dim [128]" [128] (parse_dim "[128]")
+
+let test_parse_array_type_opt () : unit =
+  let test_cases = [
+    ("int *", Some ["int"]);
+    ("int", None);
+    ("unsigned int [8][8] *", Some ["unsigned"; "int"]);
+    ("int [128]", Some ["int"]);
+    ("float[1024]", Some ["float"]);
+  ] in
+  List.iter (fun (given, expected) ->
+    Alcotest.check Alcotest.(option (list string)) ("parse_array_type_opt " ^ given) expected (parse_array_type_opt given)
+  ) test_cases
+
+let test_parse_array_dim_opt () : unit =
+  let test_cases = [
+    ("int *", None);
+    ("int", None);
+    ("unsigned int [8][8] *", Some [8; 8]);
+    ("int [128]", Some [128]);
+    ("float[1024]", Some [1024]);
+  ] in
+  List.iter (fun (given, expected) ->
+    Alcotest.check Alcotest.(option (list int)) ("parse_array_dim_opt " ^ given) expected (parse_array_dim_opt given)
+  ) test_cases
+
+let tests : unit Alcotest.test_case list =
+  [
+    ("split_array_type", `Quick, test_split_array_type);
+    ("parse_array_type_opt", `Quick, test_parse_dim);
+    ("parse_array_type_opt_1", `Quick, test_parse_array_type_opt);
+    ("parse_array_split_opt_2", `Quick, test_parse_array_dim_opt);
+  ]
+
+let () = Alcotest.run "C_type" [ ("ctype", tests) ]
---
+++ protocols/test/test_gen_z3.ml
@@ -0,0 +1,74 @@
+open Protocols
+open Protocols.Gen_z3
+open Protocols.Exp
+
+let test_optimize_expr_simple () : unit =
+  (* Test maximizing a simple constant *)
+  let result = IntGen.optimize_expr Optimizer.Strategy.Maximize (Num 42) in
+  Alcotest.check Alcotest.(result int string) "maximize constant 42" (Ok 42) result
+
+let test_optimize_expr_minimize () : unit =
+  (* Test minimizing a constant *)
+  let result = IntGen.optimize_expr Optimizer.Strategy.Minimize (Num 10) in
+  Alcotest.check Alcotest.(result int string) "minimize constant 10" (Ok 10) result
+
+let test_optimize_expr_with_variable () : unit =
+  (* Test maximizing a variable x with constraint x <= 5 *)
+  let x = Variable.from_name "x" in
+  let pre = NRel (N_rel.Le, Var x, Num 5) in
+  let result = IntGen.optimize_expr ~pre Optimizer.Strategy.Maximize (Var x) in
+  Alcotest.check Alcotest.(result int string) "maximize x where x <= 5" (Ok 5) result
+
+let test_optimize_expr_arithmetic () : unit =
+  (* Test maximizing x + 3 where x <= 2 *)
+  let x = Variable.from_name "x" in
+  let pre = NRel (N_rel.Le, Var x, Num 2) in
+  let expr = Binary (N_binary.Plus, Var x, Num 3) in
+  let result = IntGen.optimize_expr ~pre Optimizer.Strategy.Maximize expr in
+  Alcotest.check Alcotest.(result int string) "maximize x + 3 where x <= 2" (Ok 5) result
+
+let test_optimize_expr_minimize_with_constraint () : unit =
+  (* Test minimizing x where x >= 10 *)
+  let x = Variable.from_name "x" in
+  let pre = NRel (N_rel.Ge, Var x, Num 10) in
+  let result = IntGen.optimize_expr ~pre Optimizer.Strategy.Minimize (Var x) in
+  Alcotest.check Alcotest.(result int string) "minimize x where x >= 10" (Ok 10) result
+
+let test_optimize_expr_unsat () : unit =
+  (* Test with contradictory constraints: x > 5 and x < 5 *)
+  let x = Variable.from_name "x" in
+  let pre =
+    BRel
+      ( B_rel.BAnd,
+        NRel (N_rel.Gt, Var x, Num 5),
+        NRel (N_rel.Lt, Var x, Num 5) )
+  in
+  let result = IntGen.optimize_expr ~pre Optimizer.Strategy.Maximize (Var x) in
+  Alcotest.check Alcotest.(result int string) "contradictory constraints should be unsat" (Error "unsat") result
+
+let test_optimize_expr_multiplication () : unit =
+  (* Test maximizing x * 2 where x <= 3 *)
+  let x = Variable.from_name "x" in
+  let pre = NRel (N_rel.Le, Var x, Num 3) in
+  let expr = Binary (N_binary.Mult, Var x, Num 2) in
+  let result = IntGen.optimize_expr ~pre Optimizer.Strategy.Maximize expr in
+  Alcotest.check Alcotest.(result int string) "maximize x * 2 where x <= 3" (Ok 6) result
+
+let test_optimize_expr_with_timeout () : unit =
+  (* Test with timeout parameter *)
+  let result = IntGen.optimize_expr ~timeout:1000 Optimizer.Strategy.Maximize (Num 7) in
+  Alcotest.check Alcotest.(result int string) "maximize with timeout" (Ok 7) result
+
+let tests : unit Alcotest.test_case list =
+  [
+    ("optimize_expr_simple", `Quick, test_optimize_expr_simple);
+    ("optimize_expr_minimize", `Quick, test_optimize_expr_minimize);
+    ("optimize_expr_with_variable", `Quick, test_optimize_expr_with_variable);
+    ("optimize_expr_arithmetic", `Quick, test_optimize_expr_arithmetic);
+    ("optimize_expr_minimize_with_constraint", `Quick, test_optimize_expr_minimize_with_constraint);
+    ("optimize_expr_unsat", `Quick, test_optimize_expr_unsat);
+    ("optimize_expr_multiplication", `Quick, test_optimize_expr_multiplication);
+    ("optimize_expr_with_timeout", `Quick, test_optimize_expr_with_timeout);
+  ]
+
+let () = Alcotest.run "Gen_z3" [ ("test_gen_z3", tests) ]
