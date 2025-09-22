3e81ca2256b9910c2b8f18d28217a0db1725edc5
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2025-09-22 12:11:37 -0400

    Added more info on verbose mode.

--- perf_bottleneck/bin/main.ml
+++ perf_bottleneck/bin/main.ml
@@ -135,6 +135,7 @@ module Solver = struct
     |> Seq.map (fun bank ->
            let bank = Bank.normalize bank in
            let bank = if a.erase_ctx then Bank.erase_context bank else bank in
+           if a.verbose then prerr_endline(Bank.to_string bank);
            let to_cost value = Cost.from_int ~value ~exact:true () in
            let max_cost = Metric.max_cost_from a.config a.metric |> to_cost in
            let analysis_time_secs, r_cost =
