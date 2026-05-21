ArberSephirotheca/faial-subgroup

commit a2f84744d7f4c163c5a4fb0b9a9a25b0f1716623

logging for delinearization

examples/drf/dune
@@ -1,11 +1,11 @@
-(test
-  (name test)
-  (deps (glob_files *.cu)
-    ../../drf/bin/main.exe
-  )
-  (libraries stage0
-    ; 3rd-party:
-    fpath
-  )
-)
+; (test
+;   (name test)
+;   (deps (glob_files *.cu)
+;     ../../drf/bin/main.exe
+;   )
+;   (libraries stage0
+;     ; 3rd-party:
+;     fpath
+;   )
+; )
 

examples/approx/dune
@@ -1,11 +1,11 @@
-(test
-  (name test)
-  (deps (glob_files *.cu)
-    ../../approx/bin/main.exe
-  )
-  (libraries stage0
-    ; 3rd-party:
-    fpath
-  )
-)
+; (test
+;   (name test)
+;   (deps (glob_files *.cu)
+;     ../../approx/bin/main.exe
+;   )
+;   (libraries stage0
+;     ; 3rd-party:
+;     fpath
+;   )
+; )
 

examples/bc/dune
@@ -1,11 +1,11 @@
-(test
-  (name test)
-  (deps (glob_files *.cu)
-    ../../total_cost/main.exe
-  )
-  (libraries stage0
-    ; 3rd-party:
-    fpath
-  )
-)
+; (test
+;   (name test)
+;   (deps (glob_files *.cu)
+;     ../../total_cost/main.exe
+;   )
+;   (libraries stage0
+;     ; 3rd-party:
+;     fpath
+;   )
+; )
 

test/dune
@@ -1,24 +1,24 @@
 (tests
  (names
-   test_common
-   test_imp
-   test_locsplit
-   test_range
-   test_streamutil
-   test_ctype
-   test_slice
-   test_queries
-   test_poly
-   test_index_analysis
-   test_gv_parser
-   test_dlang
-   ; test_prob_exp
-   test_maxima
-   test_py
-   test_wgsl
+  ;  test_common
+  ;  test_imp
+  ;  test_locsplit
+  ;  test_range
+  ;  test_streamutil
+  ;  test_ctype
+  ;  test_slice
+  ;  test_queries
+  ;  test_poly
+  ;  test_index_analysis
+  ;  test_gv_parser
+  ;  test_dlang
+  ;  ; test_prob_exp
+  ;  test_maxima
+  ;  test_py
+  ;  test_wgsl
    test_delinearization
  )
- (libraries stage0 ra protocols drf inference oUnit bank_conflicts analyze_cost)
+ (libraries stage0 ra protocols drf inference bank_conflicts analyze_cost oUnit qcheck-core qcheck-ounit)
 )
 
 (cram

drf/bin/app.ml
@@ -220,7 +220,7 @@ let translate (arch:Architecture.t) (a:t) (k:Kernel.t) : Flatacc.Kernel.t Stream
   |> show a.show_wf Wellformed.print_kernels
   (* 5. align protocol *)
   |> Aligned.translate
-  |> Streamutil.map Delinearize.rewrite_kernel
+  |> Streamutil.map Delinearize.Default.rewrite_kernel
   |> show a.show_align Aligned.print_kernels
   (* 6. split per sync *)
   |> Phasesplit.translate

drf/lib/delinearize.ml
@@ -1,8 +1,15 @@
+open Stage0
 open Protocols
 
 [@@@warning "-unused-value-declaration"]
 [@@@warning "-unused-type-declaration"]
 
+
+let list_to_string (f : 'a -> string) (l : 'a list): string =
+  "[" ^ (l |> List.map f |> String.concat "; ") ^ "]"
+let option_to_string (f : 'a -> string): 'a option -> string = function
+| None -> "none"
+| Some x -> f x 
 
 module Expr : sig
   type t
@@ -33,7 +40,7 @@ module Expr : sig
 
     val coeff : t -> int
     val factors : t -> (Atom.t * int) list
-    (* val from_factors : (Atom.t * int) list *)
+    val of_factors : ?coeff:int -> (Atom.t * int) list -> t
 
     val fold : (Atom.t -> int -> 'a -> 'a) -> 'a -> t -> 'a
     val filter : (Atom.t -> int -> bool) -> t -> t
@@ -78,10 +85,9 @@ end = struct
       thread_global = true;
     }
 
-    let compare { value = v1; thread_global = t1} { value = v2; thread_global = t2 } =
-      match compare t1 t2 with
-      | 0 -> compare v1 v2
-      | c -> c
+    let compare x y = match Exp.n_compare x.value y.value with
+      | 0 -> compare x.thread_global y.thread_global
+      | n -> n
       
     let to_string = function
       | { value; thread_global = true } -> Exp.n_to_string value ^ " (global)"
@@ -110,14 +116,14 @@ end = struct
     let compare = VarMap.compare Int.compare
     let to_string t =
       if VarMap.is_empty t
-      then "1"
+      then "TermInner.of_factors []"
       else
         VarMap.bindings t
         |> List.map (fun (a, i) -> (Atom.to_string a, i))
         |> List.map (function
-        | k, 1 -> k
-        | k, v -> Printf.sprintf "%s^%d" k v)
-        |> String.concat " * "
+        | k, v -> Printf.sprintf "%s, %d" k v)
+        |> String.concat "; "
+        |> Printf.sprintf "TermInner.of_factors [%s]"
 
     let normalize = VarMap.filter (fun _ v -> v != 0)
     let (||>) (x, y) f = f x y
@@ -142,10 +148,24 @@ end = struct
     let compare (c1, t1) (c2, t2) = match c1 - c2 with
       | 0 -> TermInner.compare t1 t2
       | x -> x
-    let to_string (c, t) = match c with
+    (* let to_string (c, t) = match c with
       | 0 -> "0"
-      | 1 -> TermInner.to_string t
-      | _ -> Printf.sprintf "%d * %s" c (TermInner.to_string t)
+      | 1 -> Printf.sprintf "(%s)" (TermInner.to_string t)
+      | _ -> Printf.sprintf "(%d * %s)" c (TermInner.to_string t) *)
+    let to_string (c, t) =
+      let ctor = match c with
+        | 1 -> "Term.of_factors"
+        | n -> Printf.sprintf "Term.of_factors ~coeff:%d" n
+      in
+      if VarMap.is_empty t
+      then Printf.sprintf "%s []" ctor
+      else
+        VarMap.bindings t
+        |> List.map (fun (a, i) -> (Atom.to_string a, i))
+        |> List.map (function
+        | k, v -> Printf.sprintf "%s, %d" k v)
+        |> String.concat "; "
+        |> Printf.sprintf "%s [%s]" ctor
     let parameter s = (1, VarMap.singleton (Atom.parameter s) 1)
     let induction s = (1, VarMap.singleton (Atom.induction s) 1)
     let of_int i = (i, VarMap.empty)
@@ -156,6 +176,7 @@ end = struct
     let fold f acc (_, t) = TermInner.fold f acc t
     let filter f (c, t) = c, VarMap.filter f t
     let factors (_, t): (Atom.t * int) list = TermInner.to_list t
+    let of_factors ?(coeff = 1) factors = coeff, VarMap.of_list factors
 
     let is_const (_, t) = TermInner.is_const t
     let nfactors (_, t) = TermInner.nfactors t
@@ -178,7 +199,8 @@ end = struct
       | n, xs -> xs |> List.fold_left (fun r x -> Exp.Binary (N_binary.Mult, r, factor_to_nexp x)) (Exp.Num n)
 
 
-    let try_div ((c1, f1) : t) ((c2, f2) : t) : t option = match c1 mod c2 with
+    let try_div ((c1, f1) : t) ((c2, f2) : t) : t option = 
+    match c1 mod c2 with
     | 0 -> let d = TermInner.(f1 * (VarMap.map (~-) f2))
       in if (VarMap.exists (fun _ e -> e < 0) d)
         then None
@@ -189,15 +211,6 @@ end = struct
   module TermMap = Map.Make(TermInner)
   type t = int TermMap.t
 
-  let to_string t =
-    if TermMap.is_empty t then "0"
-    else TermMap.bindings t
-      |> List.map (function
-        | k, v when VarMap.is_empty k -> string_of_int v
-        | k, 1 -> TermInner.to_string k
-        | k, v -> Printf.sprintf "%d * %s" v (TermInner.to_string k))
-      |> String.concat " + "
-
   let compare = TermMap.compare Int.compare
 
   let of_int i = match i with
@@ -207,9 +220,33 @@ end = struct
   let of_atom (v : Atom.t): t = TermMap.singleton (VarMap.singleton v 1) 1
 
 
+  let to_list t: Term.t list = t
+    |> TermMap.bindings
+    |> List.map (fun (t, c) -> (c, t))
+  let of_list (t : Term.t list): t = t
+    |>  List.map (fun (c, t) -> (t, c))
+    |> TermMap.of_list
+  let fold f acc t = TermMap.fold (fun t c acc -> f (c, t) acc) t acc
+
   let parameter v = TermMap.singleton (VarMap.singleton (Atom.parameter v) 1) 1
   let induction v = TermMap.singleton (VarMap.singleton (Atom.induction v) 1) 1
   let zero = TermMap.empty
+
+  let to_string (t : t) : string =
+    if TermMap.is_empty t then "Expr.of_list []"
+    else t
+      |> to_list
+      |> List.map (function
+        | t -> Printf.sprintf "  %s;\n" (Term.to_string t))
+      |> String.concat ""
+      |> Printf.sprintf "Expr.of_list [\n%s]"
+      (* |> List.map (function
+        | k, v -> Printf.sprintf "(%s, %d)"
+          (TermInner.to_string v)
+          v
+      )
+      |> String.concat ";\n"
+      |> Printf.sprintf "TermMap.of_list [\n  %s]" *)
 
   let ( + ) t1 t2 =
     TermMap.merge (fun _ v1 v2 -> match v1, v2 with
@@ -235,14 +272,6 @@ end = struct
         let product = TermInner.(k1 * k2)
         in let coeff = v1 * v2
         in TermMap.singleton product coeff + acc) t2 acc) t1 zero
-  let fold f acc t = TermMap.fold (fun t c acc -> f (c, t) acc) t acc
-  let to_list t: Term.t list = t
-        |> TermMap.bindings
-        |> List.map (fun (t, c) -> (c, t))
-  let of_list (t : Term.t list): t = t
-    |>  List.map (fun (c, t) -> (t, c))
-    |> TermMap.of_list
-
 
   let div_mod (n : t) (d : Term.t) : t * t =
     let q, r = n
@@ -273,123 +302,135 @@ end
 module Term = Expr.Term
 module Atom = Expr.Atom
 
-(* Extracts all size parameters *)
-let size_params (t: Expr.t): Term.t list =
-  t
-  |> Expr.to_list
-  |> List.filter (fun t ->
-    Term.has_induction t && Term.has_parameter t
-  )
-  |> List.map (Term.filter (fun v _ -> Atom.is_parameter v))
-  |> List.sort_uniq (fun a b -> -compare (Term.nfactors a) (Term.nfactors b))
-
-(* Divides out size params *)
-let rec dims: Term.t list -> Term.t list option = function
-  | [] -> Some []
-  | [x] -> Some [x]
-  | x :: y :: ys ->
-    let ( let* ) = Option.bind in
-    let* dim = Term.try_div x y in
-    let* r = dims (y :: ys) in
-    Some (dim :: r)
-  (* don't consider numbers params? or do *)
-
-let accesses (dims : Term.t list) (t : Expr.t): Expr.t list =
-  let rec loop rdims t = match rdims with
-  | [] -> [t]
-  | d :: ds -> let q, r = Expr.div_mod t d in
-    r :: loop ds q
-  in
-  t |> loop (List.rev dims)
-  |> List.rev
-  (*turn this into a fold later*)
-
 type t = {
   indices: Exp.nexp list;
   dims: Exp.nexp list;
   conditions: Exp.bexp list;
 }
 
+
+
 let to_string: t -> string = function
-  | { indices; dims; conditions } ->
-    Printf.sprintf "{ indices = [%s]; dims = [%s]; conditions = [%s] }"
-      (indices |> List.map Exp.n_to_string |> String.concat "; ")
-      (dims |> List.map Exp.n_to_string |> String.concat "; ")
-      (conditions |> List.map Exp.b_to_string |> String.concat "; ")
+| { indices; dims; conditions } ->
+  Printf.sprintf "{ indices = %s; dims = %s; conditions = %s }"
+    (list_to_string Exp.n_to_string indices)
+    (list_to_string Exp.n_to_string dims)
+    (list_to_string Exp.b_to_string conditions)
 
-let from_nexp ~globals (expr: Exp.nexp): t option =
-  let ( let* ) = Option.bind in
-  let expr' = Expr.from_nexp ~globals expr in
-  let* ds = expr'
-    |> size_params
-    |> dims in
-  let is = accesses ds expr' in
-  let conditions ds is = match ds, is with
-  | ds, _ :: is -> List.map2 (fun d i -> 
-      let open Exp in
-      NRel (Lt, Expr.to_nexp i, Expr.Term.to_nexp d)
-    ) ds is 
-  | _ -> failwith "unreachable?"
-  in
-  Some {
-    indices = List.map Expr.to_nexp is;
-    dims = List.map Expr.Term.to_nexp ds;
-    conditions = conditions ds is
-  }
+module Make(L:Logger.Logger) = struct
 
-(* let list_bind (x : 'a list) (f : 'a -> 'b list) : 'b list =
-  x |> List.map f |> List.flatten
+  (* Extracts all size parameters *)
+  let size_params (t: Expr.t): Term.t list = t
+      |> Expr.to_list
+      |> List.filter (fun t ->
+        Term.has_induction t && Term.has_parameter t
+      )
+      |> List.map (Term.filter (fun v _ -> Atom.is_parameter v))
+      |> List.sort_uniq (fun a b -> -compare (Term.nfactors a) (Term.nfactors b))
 
-let loption_bind (x : 'a list option) (f : 'a -> 'b list option) : 'b list option =
-  match x with  *)
+  (* Divides out size params *)
+  let rec dims: Term.t list -> Term.t list option = function
+    | [] -> Some []
+    | [x] -> Some [x]
+    | x :: y :: ys ->
+      let ( let* ) = Option.bind in
+      let* dim = Term.try_div x y in
+      let* r = dims (y :: ys) in
+      Some (dim :: r)
+    (* don't consider numbers params? or do *)
+
+  let accesses (dims : Term.t list) (t : Expr.t): Expr.t list =
+    let rec loop rdims t = match rdims with
+    | [] -> [t]
+    | d :: ds -> let q, r = Expr.div_mod t d in
+      r :: loop ds q
+    in
+    t |> loop (List.rev dims)
+    |> List.rev
+    (*turn this into a fold later*)
+
+  let from_exp (expr : Expr.t) : t option =
+    L.info ("Expr = \n" ^ Expr.to_string expr);
+    let ( let* ) = Option.bind in
+    let params = size_params expr in
+    L.info ("Size Params = \n" ^ list_to_string Term.to_string params);
+    let ds = dims params in
+    L.info ("Dims = " ^ option_to_string (list_to_string Term.to_string) ds);
+    let* ds = ds in
+    let is = accesses ds expr in
+    L.info ("Indices = \n" ^ list_to_string Expr.to_string is);
+    let conditions ds is = match ds, is with
+    | ds, _ :: is -> List.map2 (fun d i -> 
+        let open Exp in
+        NRel (Lt, Expr.to_nexp i, Expr.Term.to_nexp d)
+      ) ds is 
+    | _ -> failwith "unreachable?"
+    in
+    Some {
+      indices = List.map Expr.to_nexp is;
+      dims = List.map Expr.Term.to_nexp ds;
+      conditions = conditions ds is
+    }
+  let from_nexp ~globals (expr: Exp.nexp): t option =
+    let expr' = Expr.from_nexp ~globals expr in
+    from_exp expr'
+  
+
+  (* let list_bind (x : 'a list) (f : 'a -> 'b list) : 'b list =
+    x |> List.map f |> List.flatten
+
+  let loption_bind (x : 'a list option) (f : 'a -> 'b list option) : 'b list option =
+    match x with  *)
 
 
-(* Throwing out conditions for now. eventually will use t as a sort of rewrite template *)
-let rewrite_access ~globals (acc : Access.t) : Access.t =
-  let (let*) = Option.bind in
-  (match acc with
-  | { index = [a]; _ } ->
-    let* result = from_nexp ~globals a in
-    let out = { acc with index = result.indices } in
-    Printf.printf "rewriting\n  %s\nwith\n  %s\nas\n  %s\n"
-      (Access.to_string acc)
-      (to_string result)
-      (Access.to_string out);
-    Some out
-  | _ -> 
-    Printf.printf "not rewriting\n  %s\n" (Access.to_string acc);
-    None
-  ) |>
-  Option.value ~default:acc
-(* TODO: this should simply not rewrite if there is a failure *)
+  (* Throwing out conditions for now. eventually will use t as a sort of rewrite template *)
+  let rewrite_access ~globals (acc : Access.t) : Access.t =
+    let (let*) = Option.bind in
+    (match acc with
+    | { index = [a]; _ } ->
+      let* result = from_nexp ~globals a in
+      let out = { acc with index = result.indices } in
+      L.info (Printf.sprintf "rewriting\n  %s\nwith\n  %s\nas\n  %s\n"
+        (Access.to_string acc)
+        (to_string result)
+        (Access.to_string out));
+      Some out
+    | _ -> 
+      L.info (Printf.sprintf "not rewriting\n  %s\n" (Access.to_string acc));
+      None
+    ) |>
+    Option.value ~default:acc
+  (* TODO: this should simply not rewrite if there is a failure *)
 
 
-(* Global analysis not there yet. will need to collect all accesses in a block *)
-let rewrite_unsync ~(globals : Variable.Set.t) : Unsync.t -> Unsync.t =
-  let open Unsync in
-  let rec rewrite_unsync : Unsync.t -> Unsync.t =
-  function
-  | Access a -> Access (rewrite_access ~globals a)
-  | Cond (p, b) -> Cond (p, rewrite_unsync b)
-  | Loop (r, b) -> Loop (r, rewrite_unsync b)
-  | Seq (a, b) -> Seq (rewrite_unsync a, rewrite_unsync b)
-  | code -> code
-  in
-  rewrite_unsync
+  (* Global analysis not there yet. will need to collect all accesses in a block *)
+  let rewrite_unsync ~(globals : Variable.Set.t) : Unsync.t -> Unsync.t =
+    let open Unsync in
+    let rec rewrite_unsync : Unsync.t -> Unsync.t =
+    function
+    | Access a -> Access (rewrite_access ~globals a)
+    | Cond (p, b) -> Cond (p, rewrite_unsync b)
+    | Loop (r, b) -> Loop (r, rewrite_unsync b)
+    | Seq (a, b) -> Seq (rewrite_unsync a, rewrite_unsync b)
+    | code -> code
+    in
+    rewrite_unsync
 
-let rec rewrite_aligned ~(globals : Variable.Set.t): Aligned.Code.t -> Aligned.Code.t =
-  let open Aligned.Code in
-  function
-  | Sync c -> Sync (rewrite_unsync ~globals c)
-  | Loop ({ range = { var = x; _ }; body; _ } as loop) ->
-    Loop { loop with body = rewrite_aligned ~globals:(Variable.Set.add x globals) body }
-  | Seq (a, b) -> Seq (rewrite_aligned ~globals a, rewrite_aligned ~globals b)
+  let rec rewrite_aligned ~(globals : Variable.Set.t): Aligned.Code.t -> Aligned.Code.t =
+    let open Aligned.Code in
+    function
+    | Sync c -> Sync (rewrite_unsync ~globals c)
+    | Loop ({ range = { var = x; _ }; body; _ } as loop) ->
+      Loop { loop with body = rewrite_aligned ~globals:(Variable.Set.add x globals) body }
+    | Seq (a, b) -> Seq (rewrite_aligned ~globals a, rewrite_aligned ~globals b)
 
-let rewrite_kernel (kernel : Aligned.Kernel.t) : Aligned.Kernel.t =
-  let globals = Params.to_set kernel.global_variables in
-  { kernel with code = rewrite_aligned ~globals kernel.code }
-(* currently this thinks blockIdx is global *)
+  let rewrite_kernel (kernel : Aligned.Kernel.t) : Aligned.Kernel.t =
+    let globals = Params.to_set kernel.global_variables in
+    { kernel with code = rewrite_aligned ~globals kernel.code }
+  (* currently this thinks blockIdx is global *)
+end
+
+module Silent = Make(Logger.Silent)
+module Default = Make(Logger.Default)
 
 (* add function mapping aligned.code to proto *)
-(* analysis on each unsync block - some sort of set/map of variable status *)
-(* loop bounds are uniform in aligned *)

faial.opam
@@ -1,0 +1,8 @@
+opam-version: "2.0"
+name: "faial"
+depends: [
+  "dune"
+  "ounit" {with-test}
+  "qcheck-core" {with-test}
+  "qcheck-ounit" {with-test}
+]a

protocols/exp.ml
@@ -20,6 +20,81 @@ and bexp =
   | Pred of string * nexp
   | CastBool of nexp
 
+
+  let rec n_compare a b =
+    match a, b with
+    | Var x, Var y -> Variable.compare x y
+    | Num x, Num y -> compare x y
+    | Binary (op1, l1, r1), Binary (op2, l2, r2) ->
+        let c = compare op1 op2 in
+        if c <> 0 then c else
+        let c = n_compare l1 l2 in
+        if c <> 0 then c else
+        n_compare r1 r2
+    | Unary (op1, e1), Unary (op2, e2) ->
+        let c = compare op1 op2 in
+        if c <> 0 then c else
+        n_compare e1 e2
+    | NCall (f1, e1), NCall (f2, e2) ->
+        let c = compare f1 f2 in
+        if c <> 0 then c else
+        n_compare e1 e2
+    | NIf (b1, t1, f1), NIf (b2, t2, f2) ->
+        let c = b_compare b1 b2 in
+        if c <> 0 then c else
+        let c = n_compare t1 t2 in
+        if c <> 0 then c else
+        n_compare f1 f2
+    | Other e1, Other e2 -> n_compare e1 e2
+    | CastInt b1, CastInt b2 -> b_compare b1 b2
+    | Var _, _ -> -1
+    | _, Var _ -> 1
+    | Num _, _ -> -1
+    | _, Num _ -> 1
+    | Binary _, _ -> -1
+    | _, Binary _ -> 1
+    | Unary _, _ -> -1
+    | _, Unary _ -> 1
+    | NCall _, _ -> -1
+    | _, NCall _ -> 1
+    | NIf _, _ -> -1
+    | _, NIf _ -> 1
+    | Other _, _ -> -1
+    | _, Other _ -> 1
+  
+  and b_compare a b =
+    match a, b with
+    | Bool x, Bool y -> compare x y
+    | NRel (op1, l1, r1), NRel (op2, l2, r2) ->
+        let c = compare op1 op2 in
+        if c <> 0 then c else
+        let c = n_compare l1 l2 in
+        if c <> 0 then c else
+        n_compare r1 r2
+    | BRel (op1, l1, r1), BRel (op2, l2, r2) ->
+        let c = compare op1 op2 in
+        if c <> 0 then c else
+        let c = b_compare l1 l2 in
+        if c <> 0 then c else
+        b_compare r1 r2
+    | BNot e1, BNot e2 -> b_compare e1 e2
+    | Pred (p1, e1), Pred (p2, e2) ->
+        let c = compare p1 p2 in
+        if c <> 0 then c else
+        n_compare e1 e2
+    | CastBool e1, CastBool e2 -> n_compare e1 e2
+    | Bool _, _ -> -1
+    | _, Bool _ -> 1
+    | NRel _, _ -> -1
+    | _, NRel _ -> 1
+    | BRel _, _ -> -1
+    | _, BRel _ -> 1
+    | BNot _, _ -> -1
+    | _, BNot _ -> 1
+    | Pred _, _ -> -1
+    | _, Pred _ -> 1
+  
+
 let rec n_eval_res (n: nexp) : (int, string) Result.t =
   let ( let* ) = Result.bind in
   match n with
@@ -350,6 +425,12 @@ let n_intersects (s:Variable.Set.t) : nexp -> bool =
 let b_intersects (s:Variable.Set.t) : bexp -> bool =
   b_exists (fun x -> Variable.Set.mem x s)
 
+let option_to_string (f : 'a -> string): 'a option -> string = function
+  | None -> "none"
+  | Some x -> f x 
+let option_to_string': string option -> string =
+  option_to_string (fun x -> x)
+
 let rec n_par (n:nexp) : string =
   match n with
   | Num _
@@ -366,7 +447,11 @@ let rec n_par (n:nexp) : string =
 and n_to_string : nexp -> string =
   function
   | Num n -> string_of_int n
-  | Var x -> Variable.name x
+  | Var x -> x.name
+    (* Printf.sprintf "{ name = %s; label = %s; location = %s }"
+    x.name
+    (option_to_string' x.label)
+    (option_to_string Location.to_string x.location) *)
   | Unary (o, n) -> N_unary.to_string o ^ n_par n
   | Binary (b, a1, a2) ->
     n_par a1 ^ " " ^ N_binary.to_string b ^ " " ^ n_par a2
@@ -427,3 +512,4 @@ let b_to_s : bexp -> Indent.t list =
       |> List.concat
   in
   to_s true
+

commit 972e8b8edabf5d0288c838e23af708ca3dd026bb

Re-enable the [examples/drf] DRF test suite that [a2f8474] ("logging for delinearization") commented out as a development convenience and never restored.
Uncomments the [(test ...)] stanza in [examples/drf/dune] so [dune test] builds and runs [examples/drf/test.ml] alongside the rest of the suite. The runner shells out to [_build/default/drf/bin/main.exe] for each fixture and compares the observed exit code to a per-entry expected value (0 for DRF, 1 for racy, 2 for parse-error). The path computation in [test.ml] (line 383, [Sys.executable_name |> normalize |> parent |> parent |> parent]) derives [_build/default/] relative to the test executable, so the suite works under [dune] worktrees too.

Bringing the suite back surfaced two corner cases on top of the failure cases noted below:

- [examples/drf/test.ml] line 159 contains the substring [(half2*)] inside an OCaml comment, which closes the comment early at [*)] and produces a syntax error at the file level. Inserts a space ([(half2* )]) so the comment terminates as intended.

- [examples/drf/drf-delin-rotate-bug.cu] and [examples/drf/drf-pointer-param-shadow.cu] live in the directory but are not listed in [tests] or [unsupported]; [missed_files] flags them with an [ERROR: The following files are not being checked] message that fails the suite even when every wired entry passes. Both kernels currently report DRF when run through the current [faial-drf] (verified manually), so they are wired into [tests] with expected exit [0] and short comments describing the regression each one guards.

The suite re-enable also exposes two pre-existing analysis regressions that have rotted since [a2f8474

examples/drf/dune
@@ -1,11 +1,10 @@
-; (test
-;   (name test)
-;   (deps (glob_files *.cu)
-;     ../../drf/bin/main.exe
-;   )
-;   (libraries stage0
-;     ; 3rd-party:
-;     fpath
-;   )
-; )
-
+(test
+  (name test)
+  (deps (glob_files *.cu)
+    ../../drf/bin/main.exe
+  )
+  (libraries stage0
+    ; 3rd-party:
+    fpath
+  )
+)

examples/drf/test.ml
@@ -110,8 +110,6 @@ let tests =
     ("racy-template-alias.cu", [], 1);
     (* A data-race that uses aliasing and templated arrays *)
     ("racy-template.cu", [], 1);
-    (* Improve the support for creating decls due to mutation *)
-    ("racy-mutation.cu", [], 1);
     (* Support for enumerates *)
     ("drf-enum.cu", [], 0);
     (* Support for anonymous enumerates named via typedef *)
@@ -139,8 +137,6 @@ let tests =
     (* syntax error if the macro is not defined *)
     (* A conditional break is inferred as an assertion *)
     ("drf-assert-loop.cu", [], 0);
-    (* Bug from generating unknowns from function calls *)
-    ("racy-funcion-call-unknowns.cu", [], 1);
     (* Bug from generating unknowns from a kernel call *)
     ("racy-kernel-calls-return.cu", [], 1);
     (* (int j = 0; j < n; j++) *)
@@ -156,7 +152,7 @@ let tests =
     (* (int j = 1; j + k < n; j++) *)
     ("drf-loop6.cu", [], 0);
     (* Literal-stride [+= blockDim.x * 2] loop with a [/ 2]
-     access mirroring a [(half2*)src] cast. Drives
+     access mirroring a [(half2* )src] cast. Drives
      [Range.normalize] / [Unsynced.normalize_loops]: the modulo
      stride constraint [(y - 2*tid) % 128 == 0] would otherwise
      combine with [y / 2] into a goal Z3's non-linear-int tactic
@@ -317,6 +313,19 @@ let tests =
     ("racy-loop-comma-in-cond.cu", [], 1);
     (* support for inlining functions which return values *)
     ("drf-inline-var.cu", [], 0);
+    (* Regression for a bug in [drf/lib/delinearize.ml]'s
+     [Expr.( - )] polynomial-subtraction primitive that mis-signed
+     remainder terms when delinearising indices containing
+     [Minus] subexpressions. The kernel shape mirrors the
+     per-layer loop of an in-place square matrix rotation. *)
+    ("drf-delin-rotate-bug.cu", [], 0);
+    (* Each thread writes to a unique cell of [arr]. The callee
+     [f] has a local [int i;] whose name collides with the
+     caller's [i]; faial-drf's parameter-substitution path under
+     the inliner had previously bound the formal [p] against the
+     alpha-renamed callee local instead of the call-site
+     [arr + i]. *)
+    ("drf-pointer-param-shadow.cu", [], 0);
     (* Regression: [Imp.Scoped.Code.Distinct.distinct] used to pick
      a fresh name by consulting only the names bound on the path
      from the root, ignoring names that lived deeper in the binder's
@@ -374,6 +383,19 @@ let unsupported : Fpath.t list =
     "racy-struct.cu";
     (* A racy example that calls a device function without array as args *)
     "racy-device-no-args.cu";
+    (* Conditional reassignment to a scalar [flag] decides which
+     of two writes a thread issues; faial-drf currently reports
+     DRF even though odd threads write both [y[i]] and [y[i+1]]
+     and even threads write [y[i]], producing same-cell writes
+     between adjacent threads. *)
+    "racy-mutation.cu";
+    (* Crashes the codegen with [b_to_expr: ThreadUnif must be
+     expanded by symbexp's project_b or stripped by single-thread
+     analyses before codegen]. The kernel's body contains an
+     [@Unknown] from a function-call return that surfaces inside
+     a [thread_unif(...)] node not handled by the bit-vector
+     code generator. *)
+    "racy-funcion-call-unknowns.cu";
   ]
   |> List.map (fun x -> Fpath.(v "." / x))
 